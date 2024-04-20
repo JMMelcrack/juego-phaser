83fa7ef10cd0bef4beb681157ef53dc3fbd52e0e
JMMelcrack <javiermm@correo.ugr.es>
2024-04-21 00:23:51 +0200

    Preloading resources

--- src/script.js
+++ src/script.js
@@ -1,5 +1,3 @@
-const Phaser = require("phaser");
-
 var config = {
     type: Phaser.AUTO,
     width: 800,
@@ -13,14 +11,21 @@ var config = {
 
 var game = new Phaser.Game(config);
 
+// Preload all the resources needed for the game
 function preload(){
-
+    this.load.image('sky', 'assets/sky.png');
+    this.load.image('ground', 'assets/platform.png');
+    this.load.image('star', 'assets/star.png');
+    this.load.image('bomb', 'assets/bomb.png');
+    this.load.spritesheet('dude', 'assets/dude.png', {frameWidth: 288/9, frameHeight: 48}); // Load the sprite
 }
 
+// Show the resources previously preloaded in the game
 function create(){
-
+    this.add.image(400, 300, 'sky');
+    this.add.image(400, 300, 'star'); // Adding the star next to the sky makes it appears above the sky in the web
 }
 
 function update(){
-    
+
 }
